5059d162b9346df572cea4f4fb8dde9a37b9ee9d
Marc Sladek <marc.sladek@synventis.com>
2013-05-30 23:04:44 +0200

    UncertainCount.hashCode(), .equals() and .toString() added

--- src/main/java/com/celements/calendar/navigation/UncertainCount.java
+++ src/main/java/com/celements/calendar/navigation/UncertainCount.java
@@ -1,5 +1,8 @@
 package com.celements.calendar.navigation;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class UncertainCount {
 
   private final int count;
@@ -16,6 +19,27 @@ public class UncertainCount {
 
   public boolean isUncertain() {
     return uncertain;
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder().append(count).append(uncertain).toHashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof UncertainCount) {
+      UncertainCount other = (UncertainCount) obj;
+      return new EqualsBuilder().append(count, other.count).append(uncertain,
+          other.uncertain).isEquals();
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "UncertainCount [count=" + count + ", uncertain=" + uncertain + "]";
   }
 
 }

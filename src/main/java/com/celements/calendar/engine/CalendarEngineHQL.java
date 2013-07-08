be91706d1397da40629f0a7503ac72fa849ee1be
Fabian Pichler <fabian.pichler@synventis.com>
2013-07-08 08:29:15 +0200

    skipping events without dates

--- src/main/java/com/celements/calendar/engine/CalendarEngineHQL.java
+++ src/main/java/com/celements/calendar/engine/CalendarEngineHQL.java
@@ -75,7 +75,13 @@ public class CalendarEngineHQL implements ICalendarEngineRole {
   private List<IEvent> convertToEventList(List<String> eventDocs) {
     List<IEvent> eventList = new ArrayList<IEvent>();
     for (String eventDocName : eventDocs) {
-      eventList.add(new Event(webUtilsService.resolveDocumentReference(eventDocName)));
+      Event event = new Event(webUtilsService.resolveDocumentReference(eventDocName));
+      if (event.getEventDate() != null) {
+        eventList.add(event);
+      } else {
+        LOGGER.warn("convertToEventList: skipping event without date [" + eventDocName
+            + "].");
+      }
     }
     return eventList;
   }

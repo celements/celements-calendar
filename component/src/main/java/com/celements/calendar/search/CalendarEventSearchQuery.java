c464c8bd913cb2595fbc65caf9736cf4db9b86bc
Marc Sladek <marc.sladek@synventis.com>
2014-05-13 17:03:59 +0200

    CalendarEventSearchQuery implemented

---
+++ component/src/main/java/com/celements/calendar/search/CalendarEventSearchQuery.java
@@ -0,0 +1,96 @@
+package com.celements.calendar.search;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.celements.calendar.classes.CalendarClasses;
+import com.celements.search.lucene.query.LuceneQueryApi;
+
+public class CalendarEventSearchQuery extends DefaultEventSearchQuery {
+
+  private static final Log LOGGER = LogFactory.getFactory().getInstance(
+      CalendarEventSearchQuery.class);
+
+  private final Date startDate;
+  private final boolean isArchive;
+  private final String lang;
+  private final List<String> allowedSpaces;
+  
+  public CalendarEventSearchQuery(String database, Date startDate, boolean isArchive, 
+      String lang, List<String> allowedSpaces, List<String> sortFields, 
+      boolean skipChecks) {
+    super(database, getDefaultSortFields(sortFields, isArchive), skipChecks);
+    this.startDate = startDate;
+    this.isArchive = isArchive;
+    this.lang = lang;
+    this.allowedSpaces = allowedSpaces;
+  }
+  
+  public CalendarEventSearchQuery(IEventSearchQuery query, Date startDate,
+      boolean isArchive, String lang, List<String> allowedSpaces) {
+    super(query, getDefaultSortFields(null, isArchive));
+    this.startDate = startDate;
+    this.isArchive = isArchive;
+    this.lang = lang;
+    this.allowedSpaces = allowedSpaces;
+  }
+
+  @Override
+  protected LuceneQueryApi getAsLuceneQueryInternal(LuceneQueryApi query) {
+    query = super.getAsLuceneQueryInternal(query);
+    query.addRestriction(getQueryService().createRestriction(
+        CalendarClasses.CALENDAR_EVENT_CLASS + "." + CalendarClasses.PROPERTY_LANG, 
+        "\"" + lang + "\""));
+    query.addOrRestrictionList(getQueryService().createRestrictionList("space", 
+        getAllowedSpaces()));
+    if (!isArchive) {
+      query.addRestriction(getQueryService().createFromDateRestriction(
+          CalendarClasses.CALENDAR_EVENT_CLASS + "." + CalendarClasses.PROPERTY_EVENT_DATE,
+          startDate, true));
+    } else {
+      query.addRestriction(getQueryService().createToDateRestriction(
+          CalendarClasses.CALENDAR_EVENT_CLASS + "." + CalendarClasses.PROPERTY_EVENT_DATE,
+          startDate, false));
+    }
+    return query;
+  }
+
+  private List<String> getAllowedSpaces() {
+    List<String> ret = new ArrayList<String>();
+    if (allowedSpaces != null && allowedSpaces.size() > 0) {
+      for(String space : allowedSpaces) {
+        ret.add("\"" + space + "\"");
+      }
+    } else {
+      // is there a nicer way to avoid returning events if there are no allowed spaces?
+      // if an empty list is returned here instead, then all events will be listed!
+      ret.add("\"NullSpace\"");
+    }
+    return ret;
+  }
+
+  @Override
+  public String toString() {
+    return "CalendarEventSearchQuery [" + super.toString() + ", startDate=" + startDate 
+        + ", isArchive=" + isArchive + ", lang=" + lang + ", allowedSpaces=" 
+        + allowedSpaces + "]";
+  }
+
+  static List<String> getDefaultSortFields(List<String> sortFields, boolean inverted) {
+    if (sortFields == null || sortFields.isEmpty()) {
+      LOGGER.info("getDefaultSortFields: got empty sortFields, using default");
+      String pref = (inverted ? "-" : "") + CalendarClasses.CALENDAR_EVENT_CLASS + ".";
+      return Arrays.asList(pref + CalendarClasses.PROPERTY_EVENT_DATE,
+          pref + CalendarClasses.PROPERTY_EVENT_DATE_END,
+          pref + CalendarClasses.PROPERTY_TITLE);
+    } else {
+      return sortFields;
+    }
+  }
+
+}
---
+++ component/src/test/java/com/celements/calendar/search/CalendarEventSearchQueryTest.java
@@ -0,0 +1,202 @@
+package com.celements.calendar.search;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.celements.common.test.AbstractBridgedComponentTestCase;
+
+public class CalendarEventSearchQueryTest extends AbstractBridgedComponentTestCase {
+  
+  private static final DateFormat SDF = new SimpleDateFormat("yyyyMMddHHmm");
+  
+  @Test
+  public void testNewCalendarEventSearchQuery() throws Exception {
+    String database = "theDB";
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    List<String> sortFields = Arrays.asList("field1", "field2");
+    boolean skipChecks = true;
+    IEventSearchQuery query = new CalendarEventSearchQuery(database, startDate, isArchive, 
+        lang, spaces, sortFields, skipChecks);
+    assertEquals(database, query.getDatabase());
+    assertEquals(sortFields, query.getSortFields());
+    assertEquals(skipChecks, query.skipChecks());
+  }
+  
+  @Test
+  public void testNewCalendarEventSearchQuery_defaultSortFields() throws Exception {
+    String database = "theDB";
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    boolean skipChecks = false;
+    IEventSearchQuery query = new CalendarEventSearchQuery(database, startDate, isArchive, 
+        lang, spaces, null, skipChecks);
+    assertEquals(database, query.getDatabase());
+    assertDefaultSortFields(query.getSortFields(), isArchive);
+    assertEquals(skipChecks, query.skipChecks());
+  }
+  
+  @Test
+  public void testNewCalendarEventSearchQuery_fromQuery() throws Exception {
+    String database = "theDB";
+    List<String> sortFields = Arrays.asList("field1", "field2");
+    boolean skipChecks = true;
+    IEventSearchQuery fromQuery = new DefaultEventSearchQuery(database, null, sortFields, 
+        skipChecks);
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    CalendarEventSearchQuery query = new CalendarEventSearchQuery(fromQuery, startDate, 
+        isArchive, lang, spaces);
+    assertEquals(database, query.getDatabase());
+    assertEquals(sortFields, query.getSortFields());
+    assertEquals(skipChecks, query.skipChecks());
+  }
+  
+  @Test
+  public void testNewCalendarEventSearchQuery_fromQuery_defaultSortFields(
+      ) throws Exception {
+    String database = "theDB";
+    boolean skipChecks = false;
+    IEventSearchQuery fromQuery = new DefaultEventSearchQuery(database, null, null, 
+        skipChecks);
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    CalendarEventSearchQuery query = new CalendarEventSearchQuery(fromQuery, startDate, 
+        isArchive, lang, spaces);
+    assertEquals(database, query.getDatabase());
+    assertDefaultSortFields(query.getSortFields(), isArchive);
+    assertEquals(skipChecks, query.skipChecks());
+  }
+  
+  @Test
+  public void testGetAsLuceneQuery() throws Exception {
+    String database = "theDB";
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    IEventSearchQuery query = new CalendarEventSearchQuery(database, startDate, isArchive, 
+        lang, spaces, null, false);
+
+    assertEquals(database, query.getDatabase());
+    String expQueryString = "object:(+\"Classes.CalendarEventClass\") "
+        + "AND Classes.CalendarEventClass.lang:(+\"de\") "
+        + "AND Classes.CalendarEventClass.eventDate:([201405090125 TO 999912312359]) "
+        + "AND (space:(+\"myCalSpace1, myCalSpace2\")) AND wiki:theDB";
+    assertEquals(expQueryString, query.getAsLuceneQuery().getQueryString());
+    assertDefaultSortFields(query.getSortFields(), isArchive);
+    assertFalse(query.skipChecks());
+  }
+  
+  @Test
+  public void testGetAsLuceneQuery_isArchive() throws Exception {
+    String database = "theDB";
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = true;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    IEventSearchQuery query = new CalendarEventSearchQuery(database, startDate, isArchive, 
+        lang, spaces, null, false);
+
+    assertEquals(database, query.getDatabase());
+    String expQueryString = "object:(+\"Classes.CalendarEventClass\") "
+        + "AND Classes.CalendarEventClass.lang:(+\"de\") "
+        + "AND Classes.CalendarEventClass.eventDate:({000101010000 TO 201405090125}) "
+        + "AND (space:(+\"myCalSpace1, myCalSpace2\")) AND wiki:theDB";
+    assertEquals(expQueryString, query.getAsLuceneQuery().getQueryString());
+    assertDefaultSortFields(query.getSortFields(), isArchive);
+    assertFalse(query.skipChecks());
+  }
+  
+  @Test
+  public void testGetAsLuceneQuery_sortFields() throws Exception {
+    String database = "theDB";
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Arrays.asList("myCalSpace1, myCalSpace2");
+    List<String> sortFields = Arrays.asList("field1", "field2");
+    boolean skipChecks = true;
+    IEventSearchQuery query = new CalendarEventSearchQuery(database, startDate, isArchive, 
+        lang, spaces, sortFields, skipChecks);
+
+    assertEquals(database, query.getDatabase());
+    String expQueryString = "object:(+\"Classes.CalendarEventClass\") "
+        + "AND Classes.CalendarEventClass.lang:(+\"de\") "
+        + "AND Classes.CalendarEventClass.eventDate:([201405090125 TO 999912312359]) "
+        + "AND (space:(+\"myCalSpace1, myCalSpace2\")) AND wiki:theDB";
+    assertEquals(expQueryString, query.getAsLuceneQuery().getQueryString());
+    assertEquals(sortFields, query.getSortFields());
+    assertTrue(query.skipChecks());
+  }
+  
+  @Test
+  public void testGetAsLuceneQuery_noSpaces() throws Exception {
+    String database = "theDB";
+    Date startDate = SDF.parse("201405090125");
+    boolean isArchive = false;
+    String lang = "de";
+    List<String> spaces = Collections.emptyList();
+    IEventSearchQuery query = new CalendarEventSearchQuery(database, startDate, isArchive, 
+        lang, spaces, null, false);
+
+    assertEquals(database, query.getDatabase());
+    String expQueryString = "object:(+\"Classes.CalendarEventClass\") "
+        + "AND Classes.CalendarEventClass.lang:(+\"de\") "
+        + "AND Classes.CalendarEventClass.eventDate:([201405090125 TO 999912312359]) "
+        + "AND (space:(+\"NullSpace\")) AND wiki:theDB";
+    assertEquals(expQueryString, query.getAsLuceneQuery().getQueryString());
+    assertDefaultSortFields(query.getSortFields(), isArchive);
+    assertFalse(query.skipChecks());
+  }
+  
+  @Test
+  public void testGetSortFields() {
+    List<String> inSortFields = Arrays.asList("field1", "field2");
+    assertEquals(inSortFields, CalendarEventSearchQuery.getDefaultSortFields(inSortFields, 
+        false));
+  }
+  
+  @Test
+  public void testGetSortFields_default() {
+    boolean inverted = false;
+    List<String> sortFields = CalendarEventSearchQuery.getDefaultSortFields(null, 
+        inverted);
+    assertDefaultSortFields(sortFields, inverted);
+  }
+  
+  @Test
+  public void testGetSortFields_default_inverted() {
+    boolean inverted = true;
+    List<String> inSortFields = Collections.emptyList();
+    List<String> sortFields = CalendarEventSearchQuery.getDefaultSortFields(inSortFields, 
+        inverted);
+    assertDefaultSortFields(sortFields, inverted);
+  }
+  
+  private void assertDefaultSortFields(List<String> sortFields, boolean inverted) {
+    String pref = inverted ? "-" : "";
+    assertNotNull(sortFields);
+    assertEquals(3, sortFields.size());
+    assertEquals(pref + "Classes.CalendarEventClass.eventDate", sortFields.get(0));
+    assertEquals(pref + "Classes.CalendarEventClass.eventDate_end", sortFields.get(1));
+    assertEquals(pref + "Classes.CalendarEventClass.l_title", sortFields.get(2));
+  }
+  
+}

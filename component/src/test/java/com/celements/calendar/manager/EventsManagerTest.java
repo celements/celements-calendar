99e2d4ddaa670a003d9b528f6a0a28865866704b
Marc Sladek <marc.sladek@synventis.com>
2014-09-18 19:27:18 +0200

    removed EventManager.updateEventObject()

--- component/src/test/java/com/celements/calendar/manager/EventsManagerTest.java
+++ component/src/test/java/com/celements/calendar/manager/EventsManagerTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +18,6 @@ import org.xwiki.model.reference.WikiReference;
 import com.celements.calendar.Calendar;
 import com.celements.calendar.ICalendar;
 import com.celements.calendar.IEvent;
-import com.celements.calendar.classes.CalendarClasses;
 import com.celements.calendar.engine.ICalendarEngineRole;
 import com.celements.calendar.search.DefaultEventSearchQuery;
 import com.celements.calendar.search.EventSearchResult;
@@ -30,10 +27,6 @@ import com.celements.common.test.AbstractBridgedComponentTestCase;
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
-import com.xpn.xwiki.doc.XWikiDocument;
-import com.xpn.xwiki.objects.BaseObject;
-import com.xpn.xwiki.objects.BaseProperty;
-import com.xpn.xwiki.objects.classes.BaseClass;
 import com.xpn.xwiki.web.Utils;
 
 public class EventsManagerTest extends AbstractBridgedComponentTestCase {
@@ -43,8 +36,6 @@ public class EventsManagerTest extends AbstractBridgedComponentTestCase {
   private XWiki xwiki;
   private ICalendarService calServiceMock;
   private ICalendarEngineRole engineMock;
-  
-  private String db;
 
   @Before
   public void setUp_EventsManagerTest() {
@@ -54,7 +45,6 @@ public class EventsManagerTest extends AbstractBridgedComponentTestCase {
     calServiceMock = createMockAndAddToDefault(ICalendarService.class);
     eventsMgr.injectCalService(calServiceMock);
     engineMock = createMockAndAddToDefault(ICalendarEngineRole.class);
-    db = "database";
   }
 
   @Test
@@ -212,279 +202,6 @@ public class EventsManagerTest extends AbstractBridgedComponentTestCase {
     replayDefault();
     assertSame(mockEventSearchResults, eventsMgr.searchEvents(calMock, query));
     verifyDefault();
-  }
-  
-  @Test
-  public void testUpdateEventObject_noChange() throws Exception {
-    XWikiDocument srcDoc = new XWikiDocument(new DocumentReference(db, "space", "src"));
-    BaseObject srcObj = getFilledCalEventObject(db);
-    srcDoc.addXObject(srcObj);
-    XWikiDocument trgDoc = new XWikiDocument(new DocumentReference(db, "space", "trg"));
-    BaseObject trgObj = getFilledCalEventObject(db);
-    trgDoc.addXObject(trgObj);
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).anyTimes();
-    
-
-    replayDefault();
-    boolean ret = eventsMgr.updateEventObject(srcDoc, trgDoc, true);
-    verifyDefault();
-    
-    assertFalse(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testUpdateEventObject_changed() throws Exception {
-    XWikiDocument srcDoc = new XWikiDocument(new DocumentReference(db, "space", "src"));
-    BaseObject srcObj = getFilledCalEventObject(db);
-    srcDoc.addXObject(srcObj);
-    XWikiDocument trgDoc = new XWikiDocument(new DocumentReference(db, "space", "trg"));
-    BaseObject trgObj = getFilledCalEventObject(db);
-    trgDoc.addXObject(trgObj);
-    trgObj.setStringValue(CalendarClasses.PROPERTY_TITLE, null);
-    boolean save = true;
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).anyTimes();
-    xwiki.saveDocument(same(trgDoc), eq("CalendarEvent updated"), eq(true), same(context));
-    expectLastCall().once();
-
-    replayDefault();
-    boolean ret = eventsMgr.updateEventObject(srcDoc, trgDoc, save);
-    verifyDefault();
-    
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testUpdateEventObject_changed_noSave() throws Exception {
-    XWikiDocument srcDoc = new XWikiDocument(new DocumentReference(db, "space", "src"));
-    BaseObject srcObj = getFilledCalEventObject(db);
-    srcDoc.addXObject(srcObj);
-    XWikiDocument trgDoc = new XWikiDocument(new DocumentReference(db, "space", "trg"));
-    BaseObject trgObj = getFilledCalEventObject(db);
-    trgDoc.addXObject(trgObj);
-    trgObj.setStringValue(CalendarClasses.PROPERTY_TITLE, null);
-    boolean save = false;
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).anyTimes();
-
-    replayDefault();
-    boolean ret = eventsMgr.updateEventObject(srcDoc, trgDoc, save);
-    verifyDefault();
-    
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testUpdateEventObject_noTrgObj() throws Exception {
-    XWikiDocument srcDoc = new XWikiDocument(new DocumentReference(db, "space", "src"));
-    BaseObject srcObj = getFilledCalEventObject(db);
-    srcDoc.addXObject(srcObj);
-    XWikiDocument trgDoc = new XWikiDocument(new DocumentReference(db, "space", "trg"));
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).anyTimes();
-    
-
-    replayDefault();
-    boolean ret = eventsMgr.updateEventObject(srcDoc, trgDoc, false);
-    verifyDefault();
-    
-    assertTrue(ret);
-    BaseObject trgObj = trgDoc.getXObject(getCalEventClassRef(db));
-    assertNotNull(trgObj);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testUpdateEventObject_noSrcObj() throws Exception {
-    XWikiDocument srcDoc = new XWikiDocument(new DocumentReference(db, "space", "src"));
-    XWikiDocument trgDoc = new XWikiDocument(new DocumentReference(db, "space", "trg"));
-    BaseObject trgObj = getFilledCalEventObject(db);
-    trgDoc.addXObject(trgObj);    
-
-    replayDefault();
-    try {
-      eventsMgr.updateEventObject(srcDoc, trgDoc, false);
-      fail("expected IllegalArgumentException");
-    } catch (IllegalArgumentException iae) {
-      // expected
-    }
-    verifyDefault();
-  }
-  
-  @Test
-  public void testUpdateEventObject_notSameDB() throws Exception {
-    XWikiDocument srcDoc = new XWikiDocument(new DocumentReference(db, "space", "src"));
-    BaseObject srcObj = getFilledCalEventObject(db);
-    srcDoc.addXObject(srcObj);
-    XWikiDocument trgDoc = new XWikiDocument(new DocumentReference(context.getDatabase(), 
-        "space", "trg"));
-    BaseObject trgObj = getFilledCalEventObject(context.getDatabase());
-    trgDoc.addXObject(trgObj);
-    trgObj.setStringValue(CalendarClasses.PROPERTY_TITLE, null);
-    boolean save = true;
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(context.getDatabase())), same(context))
-        ).andReturn(getCalEventBaseClass(context.getDatabase())).once();    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).once();
-    xwiki.saveDocument(same(trgDoc), eq("CalendarEvent updated"), eq(true), same(context));
-    expectLastCall().once();
-
-    replayDefault();
-    boolean ret = eventsMgr.updateEventObject(srcDoc, trgDoc, save);
-    verifyDefault();
-    
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testCopyBaseObject_noChange() throws Exception {
-    BaseObject srcObj = getFilledCalEventObject(db);
-    BaseObject trgObj = getFilledCalEventObject(db);
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).once();
-
-    replayDefault();
-    boolean ret = eventsMgr.copyBaseObject(srcObj, trgObj);
-    verifyDefault();
-    
-    assertFalse(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testCopyBaseObject_srcChange() throws Exception {
-    BaseObject srcObj = getFilledCalEventObject(db);
-    srcObj.setStringValue(CalendarClasses.PROPERTY_TITLE, null);
-    BaseObject trgObj = getFilledCalEventObject(db);
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).times(2);
-
-    replayDefault();
-    boolean ret = eventsMgr.copyBaseObject(srcObj, trgObj);
-    verifyDefault();
-
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testCopyBaseObject_trgChange() throws Exception {
-    BaseObject srcObj = getFilledCalEventObject(db);
-    BaseObject trgObj = getFilledCalEventObject(db);
-    trgObj.setStringValue(CalendarClasses.PROPERTY_TITLE, null);
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).times(2);
-
-    replayDefault();
-    boolean ret = eventsMgr.copyBaseObject(srcObj, trgObj);
-    verifyDefault();
-    
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testCopyBaseObject_trgEmpty() throws Exception {
-    BaseObject srcObj = getFilledCalEventObject(db);
-    BaseObject trgObj = getEmptyCalEventObject(db);
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).anyTimes();
-
-    replayDefault();
-    boolean ret = eventsMgr.copyBaseObject(srcObj, trgObj);
-    verifyDefault();
-    
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  @Test
-  public void testCopyBaseObject_srcEmpty() throws Exception {
-    BaseObject srcObj = getEmptyCalEventObject(db);
-    BaseObject trgObj = getFilledCalEventObject(db);
-    
-    expect(xwiki.getXClass(eq(getCalEventClassRef(db)), same(context))).andReturn(
-        getCalEventBaseClass(db)).anyTimes();
-
-    replayDefault();
-    boolean ret = eventsMgr.copyBaseObject(srcObj, trgObj);
-    verifyDefault();
-    
-    assertTrue(ret);
-    assertObjects(srcObj, trgObj);
-  }
-  
-  private void assertObjects(BaseObject obj1, BaseObject obj2) throws XWikiException {
-    Set<String> properties = new HashSet<String>(obj1.getPropertyList());
-    properties.addAll(obj2.getPropertyList());
-    for (String name : properties) {
-      BaseProperty prop1 = (BaseProperty) obj1.get(name);
-      Object val1 = prop1 != null ? prop1.getValue() : null;
-      BaseProperty prop2 = (BaseProperty) obj2.get(name);
-      Object val2 = prop2 != null ? prop2.getValue() : null;
-      assertEquals(val1, val2);
-    }
-  }
-  
-  private BaseObject getEmptyCalEventObject(String db) {
-    BaseObject bObj = new BaseObject();
-    bObj.setXClassReference(getCalEventClassRef(db));
-    return bObj;
-  }
-  
-  private BaseObject getFilledCalEventObject(String db) {
-    BaseObject bObj = getEmptyCalEventObject(db);
-    bObj.setStringValue(CalendarClasses.PROPERTY_LANG, "val");
-    bObj.setStringValue(CalendarClasses.PROPERTY_TITLE, "val");
-    bObj.setStringValue(CalendarClasses.PROPERTY_TITLE_RTE, "val");
-    bObj.setStringValue(CalendarClasses.PROPERTY_DESCRIPTION, "val");
-    bObj.setStringValue(CalendarClasses.PROPERTY_LOCATION, "val");
-    bObj.setStringValue(CalendarClasses.PROPERTY_LOCATION_RTE, "val");
-    bObj.setDateValue(CalendarClasses.PROPERTY_EVENT_DATE, new Date(0));
-    bObj.setDateValue(CalendarClasses.PROPERTY_EVENT_DATE_END, new Date(0));
-    bObj.setIntValue(CalendarClasses.PROPERTY_EVENT_IS_SUBSCRIBABLE, 0);
-    return bObj;
-  }
-  
-  private BaseClass getCalEventBaseClass(String db) {
-    BaseClass bclass = new BaseClass();
-    bclass.setDocumentReference(getCalEventClassRef(db));
-    bclass.addTextField(CalendarClasses.PROPERTY_LANG, CalendarClasses.PROPERTY_LANG, 30);
-    bclass.addTextField(CalendarClasses.PROPERTY_TITLE, CalendarClasses.PROPERTY_TITLE, 
-        30);
-    bclass.addTextAreaField(CalendarClasses.PROPERTY_TITLE_RTE, 
-        CalendarClasses.PROPERTY_TITLE_RTE, 80, 15);
-    bclass.addTextAreaField(CalendarClasses.PROPERTY_DESCRIPTION,
-        CalendarClasses.PROPERTY_DESCRIPTION, 80, 15);
-    bclass.addTextField(CalendarClasses.PROPERTY_LOCATION, 
-        CalendarClasses.PROPERTY_LOCATION, 30);
-    bclass.addTextAreaField(CalendarClasses.PROPERTY_LOCATION_RTE,
-        CalendarClasses.PROPERTY_LOCATION_RTE, 80, 15);
-    bclass.addDateField(CalendarClasses.PROPERTY_EVENT_DATE,
-        CalendarClasses.PROPERTY_EVENT_DATE, null, 0);
-    bclass.addDateField(CalendarClasses.PROPERTY_EVENT_DATE_END, 
-        CalendarClasses.PROPERTY_EVENT_DATE_END, null, 0);
-    bclass.addBooleanField(CalendarClasses.PROPERTY_EVENT_IS_SUBSCRIBABLE,
-        CalendarClasses.PROPERTY_EVENT_IS_SUBSCRIBABLE, "yesno");
-    return bclass;
-  }
-  
-  private DocumentReference getCalEventClassRef(String db) {
-    return new DocumentReference(db, "Classes", "CalendarEventClass");
   }
 
   private ICalendarService getCalService() {

11bb0c60021a08ca4c9145411ba14b603a63bd71
Marc Sladek <marc.sladek@synventis.com>
2014-11-24 18:22:09 +0100

    ICalendarEngineRole uses ICalendar as param

--- component/src/main/java/com/celements/calendar/engine/ICalendarEngineRole.java
+++ component/src/main/java/com/celements/calendar/engine/ICalendarEngineRole.java
@@ -1,33 +1,23 @@
 package com.celements.calendar.engine;
 
-import java.util.Date;
 import java.util.List;
 
 import org.xwiki.component.annotation.ComponentRole;
 
+import com.celements.calendar.ICalendar;
 import com.celements.calendar.IEvent;
-import com.celements.calendar.search.EventSearchResult;
-import com.celements.calendar.search.IEventSearchQuery;
 
 @ComponentRole
 public interface ICalendarEngineRole {
+  
+  public String getName();
+  
+  public long countEvents(ICalendar cal);
 
-  public long countEvents(Date startDate, boolean isArchive, String lang,
-      List<String> allowedSpaces);
+  public List<IEvent> getEvents(ICalendar cal, int offset, int limit);
 
-  public List<IEvent> getEvents(Date startDate, boolean isArchive, String lang,
-      List<String> allowedSpaces);
+  public IEvent getFirstEvent(ICalendar cal);
 
-  public List<IEvent> getEvents(Date startDate, boolean isArchive, String lang,
-      List<String> allowedSpaces, int offset, int limit);
-
-  public IEvent getFirstEvent(Date startDate, boolean isArchive, String lang,
-      List<String> allowedSpaces);
-
-  public IEvent getLastEvent(Date startDate, boolean isArchive, String lang,
-      List<String> allowedSpaces);
-
-  public EventSearchResult searchEvents(IEventSearchQuery query, Date startDate,
-      boolean isArchive, String lang, List<String> allowedSpaces);
+  public IEvent getLastEvent(ICalendar cal);
 
 }

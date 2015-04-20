53169f3ef3771747a8e7412e98490a6408d0712f
Marc Sladek <marc.sladek@synventis.com>
2015-04-20 17:11:32 +0200

    returned event list from EventSearchResult is now mutable

--- component/src/main/java/com/celements/calendar/search/EventSearchResult.java
+++ component/src/main/java/com/celements/calendar/search/EventSearchResult.java
@@ -1,7 +1,6 @@
 package com.celements.calendar.search;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -72,7 +71,7 @@ public class EventSearchResult {
             + "for search '{}'", ref, this);
       }
     }
-    return Collections.unmodifiableList(eventList);
+    return eventList;
   }
 
   public int getSize() throws LuceneSearchException {

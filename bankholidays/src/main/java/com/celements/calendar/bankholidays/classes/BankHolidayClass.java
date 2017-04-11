3c58094e52a47023838fee704c5e7039b6e99271
Nora Derguti <nora.derguti@synventis.com>
2017-04-11 14:17:47 +0200

    Regular expression added for FIELD_HOLIDAY_NAME

--- bankholidays/src/main/java/com/celements/calendar/bankholidays/classes/BankHolidayClass.java
+++ bankholidays/src/main/java/com/celements/calendar/bankholidays/classes/BankHolidayClass.java
@@ -21,17 +21,20 @@ public class BankHolidayClass extends AbstractClassDefinition implements BankHol
   public static final String CLASS_SPACE = "Classes";
   public static final String CLASS_DEF_HINT = CLASS_SPACE + "." + CLASS_NAME;
 
-  public static final ClassField<String> FIELD_HOLIDAY_DATE = new StringField.Builder(
+  public static final ClassField<String> FIELD_HOLIDAY_NAME = new StringField.Builder(
       CLASS_DEF_HINT, "holiday_name").prettyName("Holiday name").validationRegExp(
-          VALIDATION_REGEXP).validationMessage(VALIDATION_MESSAGE).size(30).build();
+          VALIDATION_REGEXP_HOLIDAY_NAME).validationMessage(VALIDATION_MESSAGE_HOLIDAY_NAME).size(
+              30).build();
 
   public static final ClassField<Date> FIELD_HOLIDAY_FROM = new DateField.Builder(CLASS_DEF_HINT,
       "holiday_from").prettyName("Holiday from").dateFormat(DATE_FORMAT_TIMESTAMP).emptyIsToday(
-          0).build();
+          0).validationRegExp(VALIDATION_REGEXP_DATE_TIME).validationMessage(
+              VALIDATION_MESSAGE_DATE_TIME).build();
 
   public static final ClassField<Date> FIELD_HOLIDAY_TO = new DateField.Builder(CLASS_DEF_HINT,
       "holiday_to").prettyName("Holiday to").dateFormat(DATE_FORMAT_TIMESTAMP).emptyIsToday(
-          0).build();
+          0).validationRegExp(VALIDATION_REGEXP_DATE_TIME).validationMessage(
+              VALIDATION_MESSAGE_DATE_TIME).build();
 
   @Override
   public String getName() {
--- bankholidays/src/main/java/com/celements/calendar/bankholidays/classes/BankHolidayClassesRole.java
+++ bankholidays/src/main/java/com/celements/calendar/bankholidays/classes/BankHolidayClassesRole.java
@@ -8,7 +8,9 @@ import com.celements.model.classes.ClassDefinition;
 public interface BankHolidayClassesRole extends ClassDefinition {
 
   public static final String DATE_FORMAT_TIMESTAMP = "dd.MM.yyyy HH:mm";
-  public static final String VALIDATION_REGEXP = "/^(0[1-9]|[12][0-9]|3[01])\\."
+  public static final String VALIDATION_REGEXP_DATE_TIME = "/^(0[1-9]|[12][0-9]|3[01])\\."
       + "(0[1-9]|1[012])\\.(19|20|99)\\d\\d ([01]?[0-9]|2[0-3]):[0-5][0-9]$/";
-  public static final String VALIDATION_MESSAGE = "bank_holiday_validation_date_time";
+  public static final String VALIDATION_MESSAGE_DATE_TIME = "bank_holiday_validation_date_time";
+  public static final String VALIDATION_REGEXP_HOLIDAY_NAME = "/^[a-zA-Z]{0,64}$/";
+  public static final String VALIDATION_MESSAGE_HOLIDAY_NAME = "bank_holiday_validation_name";
 }
